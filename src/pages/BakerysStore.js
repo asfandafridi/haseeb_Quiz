e17e69e6ab9bfa745a884a3eb378b23c83b99596
asfandafridi <asfandafridi>
2023-11-21 18:41:45 +0000

    Pushed from Locofy Builder

---
+++ src/pages/BakerysStore.js
@@ -0,0 +1,200 @@
+import styles from "./BakerysStore.module.css";
+
+const BakerysStore = () => {
+  return (
+    <div className={styles.bakerysStore}>
+      <div className={styles.heroSection}>
+        <div className={styles.indulgeInAContainer}>
+          <p
+            className={styles.indulgeInA}
+          >{`Indulge in a symphony of flavors at our artisanal bakery. `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`Welcome to a world where the aroma of freshly baked `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`wonders captivates your senses. Our master bakers `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`craft each creation with passion, using only the finest `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`ingredients to ensure every bite is pure delight. `}</p>
+          <p className={styles.indulgeInA}>&nbsp;</p>
+          <p
+            className={styles.indulgeInA}
+          >{`From flaky croissants to decadent cakes, experience `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`the epitome of taste and craftsmanship. Whether it's `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`a special occasion or a craving for the extraordinary, `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`our bakery offers a haven for your palate. Elevate `}</p>
+          <p
+            className={styles.indulgeInA}
+          >{`your moments with the magic of our oven-fresh delights. `}</p>
+          <p className={styles.indulgeInA}>
+            Your journey to exceptional taste begins here.
+          </p>
+        </div>
+        <img className={styles.image2Icon} alt="" src="/image-2@2x.png" />
+      </div>
+      <img
+        className={styles.bakerysStoreChild}
+        alt=""
+        src="/rectangle-3@2x.png"
+      />
+      <img
+        className={styles.bakerysStoreItem}
+        alt=""
+        src="/rectangle-4@2x.png"
+      />
+      <div className={styles.rectangleParent}>
+        <img className={styles.groupChild} alt="" src="/rectangle-2@2x.png" />
+        <img className={styles.groupItem} alt="" src="/rectangle-5@2x.png" />
+        <img className={styles.groupInner} alt="" src="/rectangle-6@2x.png" />
+        <img
+          className={styles.rectangleIcon}
+          alt=""
+          src="/rectangle-7@2x.png"
+        />
+      </div>
+      <b className={styles.ourOfferings}>Our offerings</b>
+      <div className={styles.barfiRs900Parent}>
+        <b className={styles.barfiRs900Container}>
+          <p className={styles.indulgeInA}>Barfi</p>
+          <p className={styles.indulgeInA}>Rs. 900/=</p>
+        </b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.orderNow}>Order Now</b>
+        </div>
+      </div>
+      <div className={styles.barfiRs900Group}>
+        <b className={styles.barfiRs900Container}>
+          <p className={styles.indulgeInA}>Barfi</p>
+          <p className={styles.indulgeInA}>Rs. 900/=</p>
+        </b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.orderNow}>Order Now</b>
+        </div>
+      </div>
+      <div className={styles.groupDiv}>
+        <b className={styles.barfiRs900Container}>
+          <p className={styles.indulgeInA}>Barfi</p>
+          <p className={styles.indulgeInA}>Rs. 900/=</p>
+        </b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.orderNow}>Order Now</b>
+        </div>
+      </div>
+      <div className={styles.barfiRs900Parent1}>
+        <b className={styles.barfiRs900Container}>
+          <p className={styles.indulgeInA}>Barfi</p>
+          <p className={styles.indulgeInA}>Rs. 900/=</p>
+        </b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.orderNow}>Order Now</b>
+        </div>
+      </div>
+      <div className={styles.barfiRs900Parent2}>
+        <b className={styles.barfiRs900Container}>
+          <p className={styles.indulgeInA}>Barfi</p>
+          <p className={styles.indulgeInA}>Rs. 900/=</p>
+        </b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.orderNow}>Order Now</b>
+        </div>
+      </div>
+      <div className={styles.barfiRs900Parent3}>
+        <b className={styles.barfiRs900Container}>
+          <p className={styles.indulgeInA}>Barfi</p>
+          <p className={styles.indulgeInA}>Rs. 900/=</p>
+        </b>
+        <div className={styles.rectangleGroup}>
+          <div className={styles.rectangleDiv} />
+          <b className={styles.orderNow}>Order Now</b>
+        </div>
+      </div>
+      <div className={styles.rectangleParent5}>
+        <div className={styles.groupChild6} />
+        <img className={styles.image3Icon} alt="" src="/image-3@2x.png" />
+        <div className={styles.atOurBakeryBakingIsntJuParent}>
+          <div className={styles.atOurBakeryContainer}>
+            <p
+              className={styles.indulgeInA}
+            >{`At our bakery, baking isn't just a process; it's an art, a passion that's woven into every loaf, pastry, and confection we create. Established with a vision to redefine indulgence, we take pride in crafting experiences that transcend the ordinary. Our story began with a small kitchen and a big dream – to infuse the world with the warmth of freshly baked goods. `}</p>
+            <p className={styles.indulgeInA}>&nbsp;</p>
+            <p className={styles.indulgeInA}>
+              What sets us apart is our unwavering commitment to quality. We
+              meticulously source the finest ingredients, from velvety chocolate
+              to locally grown fruits, ensuring that every bite tells a tale of
+              exceptional flavor. But it's not just about ingredients; it's
+              about the hands that shape them. Our dedicated team of master
+              bakers pours their heart into each recipe, combining time-honored
+              techniques with innovative twists.
+            </p>
+            <p className={styles.indulgeInA}>&nbsp;</p>
+            <p className={styles.indulgeInA}>
+              From the moment you step into our bakery, you're enveloped in a
+              symphony of scents and sights that awaken your senses. Every
+              creation that graces our shelves is a result of countless hours of
+              experimentation, dedication, and a genuine love for baking. But
+              we're more than just bakers; we're a community brought together by
+              a shared love for all things delicious.
+            </p>
+            <p className={styles.indulgeInA}>&nbsp;</p>
+            <p className={styles.indulgeInA}>
+              As we knead, roll, and bake, we're not just making pastries –
+              we're crafting memories. From birthday celebrations marked by our
+              custom cakes to lazy Sunday mornings made better with our
+              artisanal bread, we're there for life's sweetest moments. Our
+              journey is one of passion, flavor, and the joy that comes from
+              spreading happiness, one delectable treat at a time.
+            </p>
+          </div>
+          <b className={styles.aboutUs}>About Us</b>
+        </div>
+      </div>
+      <div className={styles.bakerysStoreInner} />
+      <div className={styles.bakerysStoreChild1} />
+      <b className={styles.contactUs}>Contact Us</b>
+      <div className={styles.bakerysStoreChild2} />
+      <b className={styles.fullName}>Full Name</b>
+      <div className={styles.bakerysStoreChild3} />
+      <b className={styles.phone}>Phone</b>
+      <div className={styles.bakerysStoreChild4} />
+      <b className={styles.address}>Address</b>
+      <div className={styles.bakerysStoreChild5} />
+      <b className={styles.description}>Description</b>
+      <div className={styles.bakerysStoreChild6} />
+      <b className={styles.email}>Email</b>
+      <b className={styles.submit}>Submit</b>
+      <div className={styles.bakerysStoreChild7} />
+      <b className={styles.allRightsAre}>
+        All rights are reserved by @BakeryStore
+      </b>
+      <div className={styles.rectangleParent6}>
+        <div className={styles.groupChild7} />
+        <div className={styles.groupParent}>
+          <div className={styles.homeParent}>
+            <div className={styles.home}>Home</div>
+            <div className={styles.menu}>Menu</div>
+            <div className={styles.aboutUs1}>About Us</div>
+            <div className={styles.contact}>Contact</div>
+          </div>
+          <img className={styles.image1Icon} alt="" src="/image-1@2x.png" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BakerysStore;
